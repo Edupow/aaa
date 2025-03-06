62182975ce0a95697ad5bfcbf08263aa2825356f
JOKER2l <dudu.rosalima2020@gmail.com>
2025-03-05 21:51:48 -0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SeuNome'
+description 'Recurso que carrega todas as pastas dentro da pasta stream.'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/**/*.*'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ymf'
